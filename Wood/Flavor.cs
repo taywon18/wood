2244d8008406c92a9c0a45c9c4d4d11b5376e73c
taywon18 <thespartan18@hotmail.fr>
2025-07-20 19:53:10 +0200

    added version 2.1.0

--- Wood/Flavor.cs
+++ Wood/Flavor.cs
@@ -1,6 +1,7 @@
-﻿namespace Wood
-{
+﻿using System.Collections.Generic;
 
+namespace Wood
+{
     /// <summary>
     /// Flavor should enhance readability of logs messages
     /// </summary>
@@ -30,5 +31,44 @@
         /// Used for progress
         /// </summary>
         Progress
+    }
+
+    static class Flavored
+    {
+        static object[] Important(params object[] parameters)
+        {
+            List<object> result = new List<object>(parameters.Length + 2);
+            result.Add(Flavor.Important);
+            result.AddRange(parameters);
+            result.Add(Flavor.Normal);
+            return result.ToArray();
+        }
+
+        static object[] Ok(params object[] parameters)
+        {
+            List<object> result = new List<object>(parameters.Length + 2);
+            result.Add(Flavor.Ok);
+            result.AddRange(parameters);
+            result.Add(Flavor.Normal);
+            return result.ToArray();
+        }
+
+        static object[] Failed(params object[] parameters)
+        {
+            List<object> result = new List<object>(parameters.Length + 2);
+            result.Add(Flavor.Failed);
+            result.AddRange(parameters);
+            result.Add(Flavor.Normal);
+            return result.ToArray();
+        }
+
+        static object[] Progress(params object[] parameters)
+        {
+            List<object> result = new List<object>(parameters.Length + 2);
+            result.Add(Flavor.Progress);
+            result.AddRange(parameters);
+            result.Add(Flavor.Normal);
+            return result.ToArray();
+        }
     }
 }
